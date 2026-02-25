cf76531f0b209de0a4160d6572db91950b2e095e
longps <longsydn1105@gmail.com>
2026-02-25 11:21:56 +0700

    create user repository

--- backend/repository/user_repository.go
+++ backend/repository/user_repository.go
@@ -1,8 +1,25 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"github.com/longdaica/my-app/backend/interfaces"
+	"github.com/longdaica/my-app/backend/model"
+	"gorm.io/gorm"
+)
 
-type userRepo struct {
+type userRepository struct {
 	db *gorm.DB
 }
 
+func NewUserRepository(db *gorm.DB) interfaces.UserRepositoryInterface {
+	return &userRepository{db: db}
+}
+
+func (r *userRepository) CreateUser(user *model.User) error {
+	return r.db.Create(user).Error
+}
+
+func (r *userRepository) FindByEmail(email string) (*model.User, error) {
+	var user model.User
+	err := r.db.Where("email = ?", email).First(&user).Error
+	return &user, err
+}
